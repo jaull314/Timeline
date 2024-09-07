a4ee787c6b4eca146dec5e4b61f95746ca964a10
Alex Aull <jaull314@gmail.com>
2024-09-07 17:26:42 -0500

    refactored button functionality code

--- timeline-react/src/components/SingleTimeline.jsx
+++ timeline-react/src/components/SingleTimeline.jsx
@@ -4,34 +4,25 @@ import { useRef, useState, useEffect} from "react";
 
 export default function SingleTimeline({timelineEventArr}){
     let timelineRef = useRef(new Timeline(timelineEventArr));
-    let [visiblePartOfTimeline, setVisiblePartOfTimeline] = useState(timelineRef.current.visiblePartOfTimeline);
-
-    console.log(visiblePartOfTimeline)
-    useEffect(() => {
-        const zoomOutBtn = document.getElementById("zoomOut")
-        zoomOutBtn.addEventListener("click", function(e){
-            //console.log("Before: ", timelineRef.current._unitsPerPixel)
-            timelineRef.current.zoomOutForTimeline();
-            setVisiblePartOfTimeline(timelineRef.current.visiblePartOfTimeline);
-            //console.log("After: ", timelineRef.current._unitsPerPixel)
-        })
-        const zoomInBtn = document.getElementById("zoomIn")
-        zoomInBtn.addEventListener("click", function(e){
-            //console.log("Before: ", timelineRef.current._unitsPerPixel)
-            timelineRef.current.zoomInForTimeline();
-            setVisiblePartOfTimeline(timelineRef.current.visiblePartOfTimeline);
-            //console.log("After: ", timelineRef.current._unitsPerPixel)
-        })     
-        
-    })
+    let [visibleTimelineArr, setVisibleTimelineArr] = useState(timelineRef.current.visiblePartOfTimeline);   
 
     return (
         <>
             <TimelineCanvas color={"red"} timelineObj={timelineRef.current}/>
             <div className="buttonContainer">
                 <div>
-                    <button id="zoomOut" className="zoomOut"> - </button>
-                    <button id="zoomIn" className="zoomIn"> + </button>
+                    <button id="zoomOut" className="zoomOut" onClick={
+                        () => {
+                            timelineRef.current.zoomOutForTimeline();
+                            setVisibleTimelineArr(timelineRef.current.visiblePartOfTimeline);
+                        }
+                    }> - </button>
+                    <button id="zoomIn" className="zoomIn" onClick={
+                        () => {
+                            timelineRef.current.zoomInForTimeline();
+                            setVisibleTimelineArr(timelineRef.current.visiblePartOfTimeline);
+                        }
+                    }> + </button>
                 </div>
                 <div>
                     <button className="scrollLeft"> &lt; </button>
