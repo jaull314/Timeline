a67c50722eb51b3c42be806f8532c51cdebd3ddb
Alex Aull <jaull314@gmail.com>
2024-09-17 05:37:19 -0500

    added Single/TwoTimeline(s)Screen.jsx

--- timeline-react/src/screens/TimelineSelectMenu.jsx
+++ timeline-react/src/screens/TimelineSelectMenu.jsx
@@ -1,12 +1,7 @@
-import TimelineEvent from "../TimelineClasses/TimelineEvent.js";
-import SingleTimeline from "../components/SingleTimeline.jsx"
 
 
 export default function TimelineSelectMenu(){
-    const arrTimeline = [new TimelineEvent("Declaration Of Independence", 1776),
-        new TimelineEvent("Treaty of Paris", 1783), 
-        new TimelineEvent("Start of Civil War", 1861), 
-        new TimelineEvent("End of Civil War", 1865)];
+
         
-      return <SingleTimeline timelineEventArr={arrTimeline}/>;
+      return <h1>Timeline Select Menu Filler</h1>;
 }
