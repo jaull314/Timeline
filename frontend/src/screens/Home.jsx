6c854f4dd9a6d0e16f661e2a10916bac46d969ae
Alex Aull <jaull314@gmail.com>
2024-09-22 22:11:35 -0500

    Setup Home Page

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link, useParams } from "react-router-dom";
 
 export function Home(){
   return (
-    <div>Home</div>
+    <>
+    <div id="homeDiv">
+        <h1 className="homeHeader">Timeline</h1>
+        <p>
+          Welcome to Timeline! Go to Timeline Select Menu to start creating and view your timelines.
+        </p>
+        <div id="homeLinksDiv">
+          <Link to="/TimelineSelectMenu" className="homeLinks">Timeline Select Menu</Link>
+        </div>
+      </div>
+    </>
   )
 }
 
