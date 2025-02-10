8d0c3741a7d90fd6d89b0b2d9afb10ad311ed1ba
jaull314 <76003629+jaull314@users.noreply.github.com>
2025-02-10 12:56:55 -0600

    deleted TimelineClasses folder that was a redundant copy

--- TimelineClasses/TimelineScale.js
+++
@@ -1,9 +0,0 @@
-export function calculateUnitsPerPixel(minEvent, maxEvent){
-    if(maxEvent > minEvent){
-        let timelineRange = maxEvent - minEvent;
-        let logOfRange = Math.floor(Math.log10(timelineRange));
-        return 10 ** (logOfRange - 2);
-    }else{
-        return 1;
-    }
-}
